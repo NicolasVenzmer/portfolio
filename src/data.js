56ee01664ef3abe0cc68fabd7a38e1eba25f0c00
Nicolas Venzmer <nicovenz@hotmail.com>
2021-08-06 16:19:16 -0300

    Cambios dentro de data e ingreso de la nueva pagina web de amazon clone.

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const Angular = [
     {
       id: 1,
-      title: "Web: Mis Gastos",
+      title: "Mis Gastos",
       img:
         "./assets/logo_gastos.jpg",
       url: "https://tusgastosdiarios.netlify.app/"
     },
     {
       id: 2,
-      title: "Web: Control de Empleados",
+      title: "Control de Empleados",
       img:
         "./assets/logo_empleados.jpg",
       url: "https://control-clientes-73292.web.app/home"
@@ -20,11 +20,12 @@ export const Angular = [
       id: 1,
       title: "Netflix Clone",
       img: "./assets/netflix_logo.png",
-      url: "https://netflix-clone-dca91.web.app/"
+      url: "https://netflix-clone-dca91.web.app/",
     },
     {
       id: 2,
-      title: "Coming Soon..",
-      img: "./assets/logo_mantenimiento.jpg",
+      title: "Amazon Clone",
+      img: "./assets/amazon_logo.png",
+      url: "https://amazon-clone0.netlify.app/",
     },
   ];
